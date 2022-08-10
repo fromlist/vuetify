fe67483c9fe4655fad79ecf216af989f97c56de5
Kimsungmin <fromlist5@gmail.com>
2022-08-10 22:51:49 +0900

    layout

---
+++ lsmtron/src/components/HeaderLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="header">
+    <h1>인공지능 스마트 사출시스템 V1.0.1</h1>
+    <div class="right">
+      <div class="profile"></div>
+    </div>
+  </div>
+</template>
+
+<style>
+  @import '../assets/scss/header.scss';
+</style>
---
+++ lsmtron/src/components/NavigationLayout.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="navigation">
+    
+  </div>
+</template>
+
+<style>
+  @import '../assets/scss/navigation.scss';
+</style>
